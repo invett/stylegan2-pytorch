a87f2f547357c2d785014a84c77754dba049d310
Augusto Luis Ballardini <augusto.ballardini@uah.es>
2021-03-19 15:32:19 +0100

    fixes to make projector.py work
    it will also save the embeddings as npy as txt
    
    Signed-off-by: Augusto Luis Ballardini <augusto.ballardini@uah.es>

--- projector.py
+++ projector.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
     # txtdataloader
     train_path = args.path
     dataset_ = txt_dataloader_styleGAN(train_path, decimateStep=1)
-    args.files = dataset_.images[:1]
+    args.files = dataset_.images[:]
 
     for imgfile in args.files:
         img = transform(Image.open(imgfile).convert("RGB"))
