8088eb59fb6da9b53d555316c448f83bc0a1b84f
Sandra <58729912+sandracl72@users.noreply.github.com>
2021-06-03 09:49:29 +0200

    Change pt file name

--- train.py
+++ train.py
@@ -480,7 +480,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
             if i % 1000 == 0:
                 torch.save({"g": g_module.state_dict(), "d": d_module.state_dict(), "g_ema": g_ema.state_dict(),
                             "g_optim": g_optim.state_dict(), "d_optim": d_optim.state_dict(), "args": args,
-                            "ada_aug_p": ada_aug_p, }, f"checkpoint/{str(i).zfill(6)}.pt", )
+                            "ada_aug_p": ada_aug_p, }, f"checkpoint/{wandb.run.name}_{str(i).zfill(6)}.pt", )
 
 
 if __name__ == "__main__":
